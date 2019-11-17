dfdcc490ee67f4243ae023ee09fb4fbb9c0905b8
zcbcba123 <997027407@qq.com>
2019-11-17 09:34:52 +0800

    增加json

--- src/main/java/com/zs/controller/MyController.java
+++ src/main/java/com/zs/controller/MyController.java
@@ -1,14 +1,26 @@
 package com.zs.controller;
 
+
+import com.alibaba.fastjson.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MyController {
     @ResponseBody
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello",method = RequestMethod.GET,produces = "application/json;charset=UTF-8")
     public String hello(){
-        return "HELLO...";
+        // 直接将json信息打印出来
+//        System.out.println(jsonParam.toJSONString());
+
+        // 将获取的json数据封装一层，然后在给返回
+        JSONObject result = new JSONObject();
+        result.put("msg", "ok");
+        result.put("method", "@ResponseBody");
+        result.put("data", "zz");
+
+        return result.toJSONString();
     }
 }
